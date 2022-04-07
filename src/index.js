ad5016437be8f726efd173e1b6d03b20b0ac74d9
Adam Rosowicz <adasnh@gmail.com>
2022-04-07 23:52:01 +0200

    Removed commented lines of code from index.js

--- src/index.js
+++ src/index.js
@@ -14,20 +14,3 @@ app.use(spendingRouter)
 app.listen(port, () => {
     console.log("Server is up on port " + port)
 })
-
-const Spending = require('./models/spendings')
-const User = require('./models/user')
-
-const main = async () => {
-
-
-    //const task = await Spending.findById("624f489485c1ffa33b68a4fe")
-    //await task.populate([{path: 'ownerId'}])
-    //console.log(task.ownerId)
-
-    const user = await User.findById('624f489185c1ffa33b68a4f9')
-    await user.populate([{path: 'userSpendings'}])
-    console.log(user.userSpendings)
-} 
-
-main()
